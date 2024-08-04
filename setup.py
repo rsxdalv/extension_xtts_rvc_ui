c58162cf81eefd0e324ac0a6c4c5e0c5dc7ca35f
Roberts Slisans <roberts.slisans@gmail.com>
2024-08-05 00:26:43 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+
 import setuptools
 
 # todo - fix metadata
 setuptools.setup(
-    name="extension_audiocraft_plus",
+    # name="extension_audiocraft_plus",
+	name="extension_xtts_rvc_ui",
     packages=setuptools.find_namespace_packages(),
-    version="2.0.3",
-    author="lj1995",
-    description="An easy-to-use Voice Conversion framework based on VITS",
-    url="https://github.com/RVC-Project/Retrieval-based-Voice-Conversion-WebUI",
+    # version="2.0.3",
+	version="0.0.1",
+    # author="lj1995",
+	author="rsxdalv",
+    # description="An easy-to-use Voice Conversion framework based on VITS",
+	description="XTTS-RVC-UI is a Gradio UI for XTTSv2 and RVC",
+    # url="https://github.com/RVC-Project/Retrieval-based-Voice-Conversion-WebUI",
+	url="https://github.com/rsxdalv/extension_xtts_rvc_ui",
     project_urls={},
     scripts=[],
     # include_package_data=True,

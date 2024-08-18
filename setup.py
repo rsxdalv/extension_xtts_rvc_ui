3fb85fbd733e3c12c281d78d193fa637b171044f
Roberts Slisans <roberts.slisans@gmail.com>
2024-08-18 20:19:02 +0300

    cleanup meta

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
 
 import setuptools
 
-# todo - fix metadata
 setuptools.setup(
 	name="extension_xtts_simple",
     packages=setuptools.find_namespace_packages(),
 	version="0.0.1",
 	author="rsxdalv",
 	description="XTTS-RVC-UI is a Gradio UI for XTTSv2",
-    # url="https://github.com/RVC-Project/Retrieval-based-Voice-Conversion-WebUI",
 	url="https://github.com/rsxdalv/extension_xtts_rvc_ui",
     project_urls={},
     scripts=[],
-    # include_package_data=True,
-    # include JSON files
-    # package_data={
-    #     "": ["*.json"],
-    # },
     install_requires=[
         "coqui-tts",
     ],

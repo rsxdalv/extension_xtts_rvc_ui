172f0c7be883d7af8ff2d6ef24b29267d03cb088
Roberts Slisans <roberts.slisans@gmail.com>
2024-10-13 22:35:35 +0300

    fix version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 	name="extension_xtts_simple",
     packages=setuptools.find_namespace_packages(),
-	version="0.0.1",
+	version="0.1.0",
 	author="rsxdalv",
 	description="XTTS-RVC-UI is a Gradio UI for XTTSv2",
 	url="https://github.com/rsxdalv/extension_xtts_rvc_ui",

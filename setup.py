9e94e4ba2dbf27b671610227288be816ed81e3ed
Roberts Slisans <rsxdalv@gmail.com>
2025-09-16 16:11:43 +0300

    upgrade to new distro

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 import setuptools
 
 setuptools.setup(
-	name="extension_xtts_simple",
+	name="tts_webui_extension.xtts_simple",
     packages=setuptools.find_namespace_packages(),
 	version="0.1.6",
 	author="rsxdalv",
 	description="XTTS-RVC-UI is a Gradio UI for XTTSv2",
-	url="https://github.com/rsxdalv/extension_xtts_rvc_ui",
+	url="https://github.com/rsxdalv/tts_webui_extension.xtts_rvc_ui",
     project_urls={},
     scripts=[],
     install_requires=[
@@ -19,3 +19,4 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
 )
+
